c4ff074d66a62dfac3f3d429c94163502fce186d
bethanybronkema <bethany23@ru.is>
2023-10-11 13:33:11 +0000

    work

--- tools.py
+++ tools.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 def scatter_3d_data(data1: np.ndarray, data2: np.ndarray):
     fig = plt.figure()
     ax = fig.add_subplot(111, projection='3d')
-    ax.scatter(data1[:, 0], data1[:, 1], data1[:, 2], c='k')
+    ax.scatter(data1[:, 0], data1[:, 1], data1[:, 2], c='r')
     ax.scatter(data2[:, 0], data2[:, 1], data2[:, 2], c='b')
     ax.set_xlabel('x')
     ax.set_ylabel('y')
